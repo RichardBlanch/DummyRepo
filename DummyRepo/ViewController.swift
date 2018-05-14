3cb472bf32b34ac4a67a00b3390052e41bf327bb
RichardBlanch <richard.blanchard@possible.com>
2018-05-14 09:55:32 -0600

    Initial Commit

---
+++ DummyRepo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DummyRepo
+//
+//  Created by Richard Blanchard on 5/14/18.
+//  Copyright © 2018 Richard Blanchard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
